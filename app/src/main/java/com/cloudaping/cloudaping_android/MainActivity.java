3d533dab79aff1bf5b99cfb7d5777620be734cab
Reggiecril <reggiecril0618@gmail.com>
2018-03-09 23:26:42 +0000

    changed content of navigation Page and add two cardViews.

--- app/src/main/java/com/cloudaping/cloudaping_android/MainActivity.java
+++ app/src/main/java/com/cloudaping/cloudaping_android/MainActivity.java
@@ -95,17 +95,21 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_account) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_order) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_payments) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_favourite) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_resetPassword) {
+
+        } else if (id == R.id.nav_aboutUs) {
+
+        } else if (id == R.id.nav_contactUs) {
 
         }
 
