c5698fe4bc14131cece7f535cb5bac7f096b1fe6
Reggiecril <reggiecril0618@gmail.com>
2018-03-15 01:54:57 +0000

    make the icon from navigation rotota by itself

--- app/src/main/java/com/cloudaping/cloudaping_android/ProductActivity.java
+++ app/src/main/java/com/cloudaping/cloudaping_android/ProductActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -28,6 +29,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import okhttp3.OkHttpClient;
@@ -40,10 +42,6 @@ public class ProductActivity extends AppCompatActivity
     private GridLayoutManager gridLayoutManager;
     private CustomAdapter adapter;
     private List<MyData> data_list;
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,11 +72,16 @@ public class ProductActivity extends AppCompatActivity
 
         //display the right navigation drawer
         displayRightNavigation();
+        productConnection();
 
+
+
+    }
+    public void productConnection(){
         //RecyclerView
         recyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
         data_list = new ArrayList<>();
-        load_data_from_server(0);
+        load_data_from_server("0");
 
         gridLayoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(gridLayoutManager);
@@ -91,25 +94,24 @@ public class ProductActivity extends AppCompatActivity
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
 
                 if (gridLayoutManager.findLastCompletelyVisibleItemPosition() == data_list.size() - 1) {
-                    load_data_from_server(data_list.get(data_list.size() - 1).getId());
+                    load_data_from_server(String.valueOf(data_list.get(data_list.size() - 1).getId()));
                 }
 
             }
         });
-
-
     }
+
         /**
          * load data from server
          */
-    private void load_data_from_server(int id) {
-            AsyncTask<Integer, Void, Void> task = new AsyncTask<Integer, Void, Void>() {
+    private void load_data_from_server(String id) {
+            AsyncTask<String, Void, Void> task = new AsyncTask<String, Void, Void>() {
                 @Override
-                protected Void doInBackground(Integer... integers) {
+                protected Void doInBackground(String... Strings) {
 
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("http://cloudaping.com/android/androidPopular.php?id=" + integers[0])
+                            .url("http://cloudaping.com/android/androidPopular.php?id=" + Strings[0])
                             .build();
                     try {
                         Response response = client.newCall(request).execute();
@@ -189,28 +191,64 @@ public class ProductActivity extends AppCompatActivity
                 int id = item.getItemId();
                 if(id == R.id.nav_price){
                     gallery = (LinearLayout) navigationView.getMenu().findItem(R.id.nav_price).getActionView();
-                    ImageView msg= (ImageView) gallery.findViewById(R.id.icon_up);
-                    msg.setRotation(msg.getRotation()+180);
+                    ImageView imageView= (ImageView) gallery.findViewById(R.id.icon_up);
+                    imageView.setRotation(imageView.getRotation()+180);
+                    itemId(R.id.nav_price,navigationView,gallery);
 
                 }else if (id == R.id.nav_sale) {
                     gallery = (LinearLayout) navigationView.getMenu().findItem(R.id.nav_sale).getActionView();
-                    ImageView msg= (ImageView) gallery.findViewById(R.id.icon_up);
-                    msg.setRotation(msg.getRotation()+180);
+                    ImageView imageView= (ImageView) gallery.findViewById(R.id.icon_up);
+                    imageView.setRotation(imageView.getRotation()+180);
+                    itemId(R.id.nav_sale,navigationView,gallery);
 
                 } else if (id == R.id.nav_review) {
                     gallery = (LinearLayout) navigationView.getMenu().findItem(R.id.nav_review).getActionView();
-                    ImageView msg= (ImageView) gallery.findViewById(R.id.icon_up);
-                    msg.setRotation(msg.getRotation()+180);
+                    ImageView imageView= (ImageView) gallery.findViewById(R.id.icon_up);
+                    imageView.setRotation(imageView.getRotation()+180);
+                    itemId(R.id.nav_review,navigationView,gallery);
 
                 } else if (id == R.id.nav_updateTime) {
                     gallery = (LinearLayout) navigationView.getMenu().findItem(R.id.nav_updateTime).getActionView();
-                    ImageView msg= (ImageView) gallery.findViewById(R.id.icon_up);
-                    msg.setRotation(msg.getRotation()+180);
+                    ImageView imageView= (ImageView) gallery.findViewById(R.id.icon_up);
+                    imageView.setRotation(imageView.getRotation()+180);
+                    itemId(R.id.nav_updateTime,navigationView,gallery);
                 }
 //                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 //                drawer.closeDrawer(GravityCompat.END);
                 return true;
 
+            }
+
+            /**
+             * find the other three item when click one item, then reset them.
+             * @param resource
+             * @param navigationView
+             * @param gallery
+             */
+            public void itemId(int resource,NavigationView navigationView,LinearLayout gallery){
+                int []allResource=new int[]{R.id.nav_price,R.id.nav_sale,R.id.nav_review,R.id.nav_updateTime};
+                HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+                ImageView imageView1= (ImageView) gallery.findViewById(R.id.icon_up);
+                imageView1.setImageResource(R.drawable.ic_arrow_upward_black_24dp);
+
+                for (int i=0;i<allResource.length;i++){
+                    map.put(allResource[i],allResource[i]);
+                }
+                if(map.get(resource)==resource){
+                    map.remove(resource);
+                }else{
+                    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                    drawer.closeDrawer(GravityCompat.START);
+                }
+
+
+
+                for (Integer value : map.values()){
+
+                    gallery = (LinearLayout) navigationView.getMenu().findItem(value).getActionView();
+                    ImageView imageView= (ImageView) gallery.findViewById(R.id.icon_up);
+                    imageView.setImageResource(R.drawable.ic_remove_black_24dp);
+                }
             }
         });
     }
